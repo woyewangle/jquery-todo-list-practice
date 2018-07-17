5b192ac5d13b65114b3a73f1f5f59110ae6a2d72
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-18 01:57:43 +0800

    had fins it but haven'th use react to refactor it

--- js/todo.js
+++ js/todo.js
@@ -21,5 +21,65 @@ $(document)
             return uuid;
         }
 
+        $('#button').click(function(){
+            let content = $("input[name='ListItem']").val();
+            let id=generateUUID();
+            if(content){
+                $("ol").append (`<li id=${id} class=""><input name="done-todo" type="checkbox" class="done-todo"> ${content} </li>`);
+            }
+            $("input.input-text").val("");
+
+        });
+
+
+        $("ol li").on("click",function(){
+            let currentClass = $(this).attr("class");
+            //alert(currentClass);
+            if(currentClass == "checked"){
+                $(this).removeClass();
+            }else {
+                $(this).addClass("checked");
+            }
+        })
+
+        $("ol li").click(function(){
+            $(this).attr("contentEditable",true);
+
+        })
+
+
+        $("a[data-filter=all]").click(function(){
+            $(this).addClass("selected");
+            $("ol li").css("display","");
+        });
+
+        $("a[data-filter=active]").click(function(){
+            $(this).addClass("selected");
+            $("ol").children().filter('.checked').css("display","none")
+            $("ol").children().not('.checked').css("display","")
+        });
+
+        $("a[data-filter=complete]").click(function(){
+            $(this).addClass("selected");
+            $("ol").children().not('.checked').css("display","none")
+            $("ol").children().filter('.checked').css("display","")
+        });
+
+
+        
+
+     
         // code to be implemented
     });
+    
+    
+
+  
+    
+   
+    
+    
+       
+
+    
+
